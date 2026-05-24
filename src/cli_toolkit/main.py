c300c948356651176f33dbb4b708590cb05484a9
Aviral Joshi <aviraljoshi2718@gmail.com>
2026-05-24 02:20:44 +0000

    feat: replace print with logging in main.py

--- src/cli_toolkit/main.py
+++ src/cli_toolkit/main.py
@@ -1,16 +1,23 @@
 import sys
 from cli_toolkit.utils import number_utils as nu
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 def main():
     if len(sys.argv) == 2:
         num = int(sys.argv[1])
+        logger.debug(f"Received argument : {num}")
         even_odd = nu.is_even_or_odd(num)
-        print(f"Given number {num} is {even_odd}")
+        logger.info(f"Given number {num} is {even_odd}")
         factorial = nu.factorial(num)
-        print(f"Factorial of the given number {num} is {factorial}")
+        logger.info(f"Factorial of the given number {num} is {factorial}")
+        if num > 10:
+            logger.warning(f"Large number {num} -- Factorial will be very big")
     else:
-        print(f"correct usage: python {sys.argv[0]} [num]")
+        logger.error(f"correct usage: python {sys.argv[0]} [num]")
 
 
 if __name__ == "__main__":
